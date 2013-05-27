a4879c30feefd76e11cd19cf73db26762f6c740f
Drew Barontini <drewbarontini@gmail.com>
2013-05-27 18:43:15 -0400

    Update make_symlinks.sh

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/Sites/dotfiles                    # dotfiles directory
-olddir=~/Sites/dotfiles_old             	# old dotfiles backup directory
-files="bash_profile gitignore gitconfig vimrc"    # list of files/folders to symlink in homedir
-
-##########
+dir=~/Sites/dotfiles
+olddir=~/Sites/dotfiles_old
+files="bash_profile gitignore gitconfig vimrc"
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -22,7 +14,7 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
