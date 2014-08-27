f807bd22dea1d827a7e79dd13d3df6a13ad4b168
Drew Barontini <drewbarontini@gmail.com>
2014-08-27 09:29:35 -0400

    Comment bash functions, better comments

--- install.sh
+++ install.sh
@@ -2,31 +2,46 @@
 
 if [ ! -d "$HOME/.dotfiles" ]
 then
-  echo "Installing Drew Barontini's dotfiles..."
+  echo
+  echo "=============================================================="
+  echo "==> Installing Drew Barontini's dotfiles..."
+  echo "=============================================================="
+  echo
   git clone git@github.com:drewbarontini/dotfiles.git ~/.dotfiles
 
-  echo "Setting up Bash..."
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Bash..."
+  echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/bash ~/.bash
   ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
-  echo "Setting up Ack..."
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Ack..."
+  echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/ack/ackrc ~/.ackrc
 
-  echo "Setting up Git..."
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Git..."
+  echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/git/gitconfig.local ~/.gitconfig.local
   ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
   ln -s ~/.dotfiles/git/gitignore ~/.gitignore
 
-  echo "Setting up Sublime Text..."
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Sublime Text..."
+  echo "--------------------------------------------------------------"
   cd ~/Library/Application\ Support/Sublime\ Text\ 3/
   ln -s ~/.dotfiles/sublime-text/Packages Packages
   ln -s /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
-  echo "Setting up Vim..."
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Vim..."
+  echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/vim ~/.vim
   ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
   ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  echo
   echo "Run :BundleInstall in Vim to install plugins"
 
   source ~/.bash_profile
