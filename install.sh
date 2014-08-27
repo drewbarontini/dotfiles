eeeab3d77e8524a82d4de731c1b8da7c415063fb
Drew Barontini <drewbarontini@gmail.com>
2014-08-27 10:46:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,39 +14,57 @@ then
   echo "--> Setting up Bash..."
   echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/bash ~/.bash
+  echo "--> Symlinked ~/.dotfiles/bash to ~/.bash"
   ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+  echo "--> Symlinked ~/.dotfiles/bash_profile to ~/.bash_profile"
 
+  echo
   echo "--------------------------------------------------------------"
   echo "--> Setting up Ack..."
   echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/ack/ackrc ~/.ackrc
+  echo "--> Symlinked ~/.dotfiles/ack/ackrc to ~/.ackrc"
 
+  echo
   echo "--------------------------------------------------------------"
   echo "--> Setting up Git..."
   echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/git/gitconfig.local ~/.gitconfig.local
+  echo "--> Symlinked ~/.dotfiles/git/gitconfig.local to ~/.gitconfig.local"
   ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+  echo "--> Symlinked ~/.dotfiles/git/gitconfig to ~/.gitconfig"
   ln -s ~/.dotfiles/git/gitignore ~/.gitignore
+  echo "--> Symlinked ~/.dotfiles/git/gitignore to ~/.gitignore"
 
+  echo
   echo "--------------------------------------------------------------"
   echo "--> Setting up Sublime Text..."
   echo "--------------------------------------------------------------"
   cd ~/Library/Application\ Support/Sublime\ Text\ 3/
   ln -s ~/.dotfiles/sublime-text/Packages Packages
+  echo "--> Symlinked ~/.dotfiles/sublime-text/Packages Packages to Sublime Text Packages"
   ln -s /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+  echo "--> Symlinked Sublime Text subl to /usr/local/bin/subl"
 
+  echo
   echo "--------------------------------------------------------------"
   echo "--> Setting up Vim..."
   echo "--------------------------------------------------------------"
   ln -s ~/.dotfiles/vim ~/.vim
+  echo "--> Symlinked ~/.dotfiles/vim to ~/.vim"
   ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+  echo "--> Symlinked ~/.dotfiles/vim/vimrc to ~/.vimrc"
   ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
+  echo "--> Symlinked ~/.dotfiles/vim/gvimrc to ~/.gvimrc"
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  echo "--> Cloned gmarik/vundle to ~/.vim/bundle/vundle"
   echo
   echo "Run :BundleInstall in Vim to install plugins"
 
   source ~/.bash_profile
+  echo "--> Sourced ~/.bash_profile"
 
+  echo
   echo "Done! Run 'open ~/.dotfiles' to see your new files."
 else
   echo "These dotfiles are already installed on this machine."
