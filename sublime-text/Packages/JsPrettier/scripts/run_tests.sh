0ff0b1ae46b9d403896fe577540a25c1bdcca36f
Drew Barontini <drewbarontini@gmail.com>
2017-09-14 07:27:58 -0400

    Update Sublime Text stuff

--- sublime-text/Packages/JsPrettier/scripts/run_tests.sh
+++ sublime-text/Packages/JsPrettier/scripts/run_tests.sh
@@ -1,20 +1,57 @@
 #!/usr/bin/env bash
 
 set -e
+[ "$TRAVIS" == "true" ] && set -x
 
 readonly SCRIPTSDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
-echo
-echo '> Run tests'
-echo
 
-pushd "${SCRIPTSDIR}" && pushd ..
-py.test .
-flake8 .
-pylint JsPrettier.py
-markdownlint .
-popd && popd
+cd_project_root() {
+    echo '> cd to project root'
+    pushd "${SCRIPTSDIR}" && pushd ..
+}
 
-echo
-echo 'Finished.'
-echo
+cd_previous_working_dir() {
+    echo '> Restore previous working directory'
+    popd && popd
+    echo
+}
+
+run_pytest() {
+    echo
+    echo '> Run pytest'
+    py.test .
+}
+
+run_flake8() {
+    echo
+    echo '> Run flake8'
+    echo -n 'Total errors: '
+    flake8 . --count
+}
+
+run_pylint() {
+    echo
+    echo -n '> Run pylint'
+    pylint JsPrettier.py
+}
+
+run_markdownlint() {
+    echo -n '> Run markdownlint'
+    markdownlint .
+    echo 'Markdown looks good.' && echo
+}
+
+
+main() {
+    cd_project_root
+    run_pytest
+    run_flake8
+    run_pylint
+    run_markdownlint
+    cd_previous_working_dir
+
+    echo & echo 'Finished.'
+}
+
+main
