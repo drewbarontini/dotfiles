e3d9d2fe631ea35fa88c241c67a709e29e471b9b
Drew Barontini <hello@drewb.io>
2025-09-07 14:15:52 -0400

    Update LazyVim color scheme

---
+++ neovim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "projekt0n/github-nvim-theme" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_tritanopia", -- Set the default colorscheme
+    },
+  },
+}
